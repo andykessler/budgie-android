76e53b9c46fc7834aaafe732d321b45bedd27829
Andy Kessler <akkessler@users.noreply.github.com>
2017-12-24 22:16:39 -0600

    Create custom dialog layout

--- app/src/main/java/io/akessler/budgie/android/CategoryCreateDialog.java
+++ app/src/main/java/io/akessler/budgie/android/CategoryCreateDialog.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 
 public class CategoryCreateDialog extends DialogFragment {
 
@@ -13,19 +14,30 @@ public class CategoryCreateDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Title")
-                .setPositiveButton("Positive", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        System.out.println("TODO Positive");
-                    }
-                })
-                .setNegativeButton("Negative", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        System.out.println("TODO Negative");
-                    }
-                });
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_category_create, null))
+                .setPositiveButton("Positive", positiveOnClickListener())
+                .setNegativeButton("Negative", negativeOnClickListener());
         // Create the AlertDialog object and return it
         return builder.create();
+    }
+
+    private DialogInterface.OnClickListener positiveOnClickListener() {
+        return new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                System.out.println("TODO Positive");
+            }
+        };
+    }
+
+    private DialogInterface.OnClickListener negativeOnClickListener() {
+        return new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                System.out.println("TODO Negative");
+            }
+        };
     }
 
 }
